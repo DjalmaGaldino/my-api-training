1adca6bc2e52a22060eff7254d7787b4bac6f6e5
Djalma Galdino <galdino.dj@gmail.com>
2023-10-11 20:13:20 -0300

    adding route roles post

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,12 +1,14 @@
-import { AppError } from "@shared/errors/AppError"
 import { Router } from "express"
+import { rolesRouter } from "@roles/http/routes/roles.routes"
 
 const routes = Router()
 
 routes.get('/', (request, response) => {
-  throw new Error('Acesso negado')
+  // throw new Error('Acesso negado')
   return response.json({ message: 'Hello Dev!' })
 })
+
+routes.use('/roles', rolesRouter)
 
 
 export { routes }
