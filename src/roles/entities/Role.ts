cdd4ec93f00475fe47af9b1c80655b57a688bf66
Djalma Galdino <galdino.dj@gmail.com>
2023-10-13 14:32:05 -0300

    Refactory role entity

--- src/roles/entities/Role.ts
+++ src/roles/entities/Role.ts
@@ -1,8 +1,16 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
 import { v4 as uuidv4 } from 'uuid'
 
+
+@Entity('roles')
 export class Role {
+  @PrimaryColumn()
   id?: string
+
+  @Column()
   name: string
+
+  @CreateDateColumn()
   created_at: Date
 
   constructor() {
