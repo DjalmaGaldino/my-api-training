8817517dfa40bb7761c49e9ebaa0dfae6b6f66e1
Djalma Galdino <galdino.dj@gmail.com>
2023-10-16 19:36:08 -0300

    Creating migration CreateRefreshTokensTable

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -4,6 +4,7 @@ import { Role } from '@roles/entities/Role'
 import { CreateUsersTable1697320339993 } from './migrations/1697320339993-CreateUsersTable'
 import { AddRoleIdToUsersTable1697320807837 } from './migrations/1697320807837-AddRoleIdToUsersTable'
 import { User } from '@users/entities/User'
+import { CreateRefreshTokensTable1697495219730 } from './migrations/1697495219730-CreateRefreshTokensTable'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
@@ -13,5 +14,6 @@ export const dataSource = new DataSource({
     CreateRolesTable1697216413192,
     CreateUsersTable1697320339993,
     AddRoleIdToUsersTable1697320807837,
+    CreateRefreshTokensTable1697495219730
   ],
 })
