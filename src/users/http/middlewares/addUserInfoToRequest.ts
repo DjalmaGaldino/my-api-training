839d6c72c6dee63af2f5533fe6a0f31541e53ce0
Djalma Galdino <galdino.dj@gmail.com>
2023-10-16 20:32:54 -0300

    Creating addUserInfoToRequest middleware

---
+++ src/users/http/middlewares/addUserInfoToRequest.ts
@@ -0,0 +1,49 @@
+import { AppError } from '@shared/errors/AppError'
+import { NextFunction, Request, Response } from 'express'
+import { Secret, decode, verify } from 'jsonwebtoken'
+import authConfig from '@config/auth'
+
+type JwtPayloadProps = {
+  sub: string
+}
+
+export const addUserInfoToRequest = (
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) => {
+  const authHeader = request.headers.authorization
+
+  if (!authHeader) {
+    return response.status(401).json({
+      error: true,
+      code: 'token.invalid',
+      message: 'Access token not present'
+    })
+  }
+
+  const token = authHeader.replace('Bearer', '')
+
+  if (!token) {
+    return response.status(401).json({
+      error: true,
+      code: 'token.invalid',
+      message: 'Access token not present'
+    })
+  }
+
+  try {
+    const decodedToken = decode(token)
+    const { sub } = decodedToken as JwtPayloadProps
+    request.user = { id: sub }
+
+    return next()
+
+  } catch (error) {
+    return response.status(401).json({
+      error: true,
+      code: 'token.invalid',
+      message: 'Access token not present'
+    })
+  }
+}
