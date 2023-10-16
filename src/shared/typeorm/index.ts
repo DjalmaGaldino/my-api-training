cf1fe650096c3beace21bee084b135f4e8a428eb
Djalma Galdino <galdino.dj@gmail.com>
2023-10-16 19:47:33 -0300

    Creating RefreshToken entity

--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -5,11 +5,12 @@ import { CreateUsersTable1697320339993 } from './migrations/1697320339993-Create
 import { AddRoleIdToUsersTable1697320807837 } from './migrations/1697320807837-AddRoleIdToUsersTable'
 import { User } from '@users/entities/User'
 import { CreateRefreshTokensTable1697495219730 } from './migrations/1697495219730-CreateRefreshTokensTable'
+import { RefreshToken } from '@users/entities/RefreshToken'
 
 export const dataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [Role, User],
+  entities: [Role, User, RefreshToken],
   migrations: [
     CreateRolesTable1697216413192,
     CreateUsersTable1697320339993,
---
+++ src/users/entities/RefreshToken.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+} from 'typeorm'
+import { v4 as uuidv4 } from 'uuid'
+
+@Entity('refresh_tokens')
+export class RefreshToken {
+  @PrimaryColumn()
+  id?: string
+
+  @Column()
+  token: string
+
+  @Column()
+  valid: boolean
+
+  @Column()
+  user_id: string
+
+  @Column()
+  expires: Date
+
+  @CreateDateColumn()
+  created_at: Date
+
+  // toda vez que se instanciar esta classe o construtor será executado e verifica se tem ou não o id e cria caso não tenha
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4()
+    }
+  }
+}
